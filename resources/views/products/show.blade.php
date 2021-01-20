e36a92381976b0fd0ee39e9cc69a4dee3c81a138
Ivan Mincione <ivan.mincione92@outlook.it>
2021-01-20 16:56:27 +0100

    Print card of single product

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,1 +1,22 @@
 @extends("layouts.app")
+
+{{-- stampare scheda del singolo prodotto --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="..." alt="Codice Prodotto {{ $product->id }}">
+                  <div class="card-body">
+                    <h5 class="card-title">{{ $product->name }}</h5>
+                    <p>Prezzo: {{ $product->price }}</p>
+                    <p class="card-text">Descrizione: {{ $product->description }}</p>
+                    <a href="#" class="btn btn-primary">Acquista</a>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
