273e0cc9b11369a2718f0acbabb515761cd94eae
Ivan Mincione <ivan.mincione92@outlook.it>
2021-01-20 15:55:27 +0100

    Add HomeController and test products.index

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="menu-header">
+        <ul>
+            <li>
+                <a class="uppercase" href="{{ route("homepage") }}">home</a>
+            </li>
+            <li>
+                <a class="uppercase" href="{{ route("products.index") }}">products</a>
+            </li>
+        </ul>
+    </div>
+</header>
